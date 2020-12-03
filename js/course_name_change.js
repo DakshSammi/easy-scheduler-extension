eafb3b557bb9ecb37e9bebff8c7a6d53265967da
osheensachdev <osheen18059@iiitd.ac.in>
2020-12-04 03:13:25 +0530

    check if course in database and show/remove bell accordingly

--- js/course_name_change.js
+++ js/course_name_change.js
@@ -1,15 +1,30 @@
 async function onCourseNameChange(course_name) {
-	// check student schedule
-	var check_student_schedule = `${DEADLINE_SCHEDULING_SUGGESTION_API}/${COLLEGE_NAME}/student_schedule/${course_name}/week`;
-	response = await fetch(check_student_schedule)
-	const score = await response.json();
-	console.log(score);
-	var settings = document.getElementsByClassName('fB7J9c kWv2Xb QRiHXd')[0];
-	if(score.score == 0 && !settings.parentNode.contains(alert_bell_div) && alert_bell_div!=undefined) {
-		settings.parentNode.insertBefore(alert_bell_div, settings);		
-	} else if(score.score > 0 && settings.parentNode.contains(alert_bell_div)){
-		settings.parentNode.removeChild(alert_bell_div);
+	// check if course is present in the database
+	var is_present = `${DEADLINE_SCHEDULING_SUGGESTION_API}/${COLLEGE_NAME}/courses/${course_name}/is_present`
+	var response = await fetch(is_present)
+	is_present = (await response.text()) == 'true'
+
+	console.log(is_present)
+	// if not first get the course added
+	if(is_present) {
+		var check_student_schedule = `${DEADLINE_SCHEDULING_SUGGESTION_API}/${COLLEGE_NAME}/student_schedule/${course_name}/week`;
+		response = await fetch(check_student_schedule)
+		const score = await response.json();
+		console.log(score);
+		var settings = document.getElementsByClassName('fB7J9c kWv2Xb QRiHXd')[0];
+		if(score.score == 0 && !settings.parentNode.contains(alert_bell_div) && alert_bell_div!=undefined) {
+			settings.parentNode.insertBefore(alert_bell_div, settings);		
+		} else if(score.score > 0 && settings.parentNode.contains(alert_bell_div)){
+			settings.parentNode.removeChild(alert_bell_div);
+		}
+	} else {
+		var settings = document.getElementsByClassName('fB7J9c kWv2Xb QRiHXd')[0];
+		if(settings.parentNode.contains(alert_bell_div)) {
+			settings.parentNode.removeChild(alert_bell_div);
+		}
 	}
+	// check student schedule
+	
 }
 
 async function promptUserToAddCourse() {
