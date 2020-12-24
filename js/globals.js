2439fcb47990311d47a8b683a8a5a01fa48743d7
osheensachdev <osheen18059@iiitd.ac.in>
2020-12-24 13:26:44 +0530

    migrated to VM

--- js/globals.js
+++ js/globals.js
@@ -1,10 +1,10 @@
 var course_name = null;
 const BUTTON_CLASS = "U26fgb cd29Sd p0oLxb BEAGS CG2qQ QRiHXd bVp04e center";
 const HEADING_CLASS = "gtuckc tLDEHd-Wvd9Cc";
-// const DEADLINE_SCHEDULING_SUGGESTION_API = 'http://103.25.231.35:8200'
-// const COLLEGE_NAME = 'IIIT Delhi' 
-const DEADLINE_SCHEDULING_SUGGESTION_API = 'https://deadline-scheduling-suggestion.herokuapp.com'
-const COLLEGE_NAME = 'iiitd' 
+const DEADLINE_SCHEDULING_SUGGESTION_API = 'https://easyscheduler.kracr.iiitd.edu.in:8200'
+const COLLEGE_NAME = 'IIIT Delhi' 
+// const DEADLINE_SCHEDULING_SUGGESTION_API = 'https://deadline-scheduling-suggestion.herokuapp.com'
+// const COLLEGE_NAME = 'iiitd' 
 
 function fetch_ (url, options, timeout = 10000) {
     return Promise.race([
